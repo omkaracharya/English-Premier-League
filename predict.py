84d47e1b7f2b3d06e8a6f9daecb7a054a448e03a
Omkar Acharya <acharyaomkar01@gmail.com>
2017-03-08 17:21:42 -0500

    Author removed

--- predict.py
+++ predict.py
@@ -1,8 +1,6 @@
 '''
 Predicting the final match result based on the half time score
 Features used: { half time score, # total shots, # shots on target }
-
-Author: Omkar Acharya
 '''
 
 from sklearn.metrics import accuracy_score
